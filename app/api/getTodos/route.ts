2f31ff80e9668594118df6a60a60f55ac89695fe
Dimitar <dimitar.mandarliev@gmail.com>
2024-01-24 15:55:27 +0200

    edited the post and get requests

--- app/api/getTodos/route.ts
+++ app/api/getTodos/route.ts
@@ -2,5 +2,28 @@ export async function GET(request: Request) {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const todos = await res.json();
 
-  return Response.json({ todos });
+  const newTodos = [
+    {
+      id: 0,
+      title: "GET REQUEST DEMO",
+    },
+    ...todos,
+  ];
+
+  return Response.json({ todos: newTodos });
+}
+
+export async function POST(request: Request) {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const todos = await res.json();
+
+  const newTodos = [
+    {
+      id: 0,
+      title: "POST REQUEST DEMO",
+    },
+    ...todos,
+  ];
+
+  return Response.json({ todos: newTodos });
 }
